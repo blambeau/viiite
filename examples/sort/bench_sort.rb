3d431f7a72f6b2fe69db17acbf3f70b9d391b53f
Bernard Lambeau <blambeau@gmail.com>
2011-06-01 18:05:25 +0200

    Added ruby version as global _by_

--- examples/sort/bench_sort.rb
+++ examples/sort/bench_sort.rb
@@ -5,7 +5,7 @@ require File.expand_path("../sort", __FILE__)
 Bench.runner do |b|
   b.variation_point :ruby_version, Bench.short_ruby_descr
   b.range_over([1, 50, 100, 150, 200, 250, 300], :size) do |size|
-    1.times do 
+    10.times do 
       array = Array.new(size){ Kernel.rand }
       b.variation_point :test, :quicksort do
         b.report{ quicksort(array) }
