be61723412d4635bd9a2a852d69770651fa98ad0
Bernard Lambeau <blambeau@gmail.com>
2011-06-20 20:20:46 +0200

    Removed aggregator, as Alf does far better

--- lib/bench.rb
+++ lib/bench.rb
@@ -35,7 +35,6 @@ module Bench
     
 end # module Bench
 require "bench/runner"
-require "bench/aggregator"
 require "bench/inputter/hash_reader"
 require "bench/formatter/text"
 require "bench/formatter/plot"
