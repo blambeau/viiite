ef2bfe8444cccc10e7ddc76e58c44116d2beaea7
Bernard Lambeau <blambeau@gmail.com>
2011-08-23 13:13:07 +0200

    Added a fixture bench

---
+++ spec/fixtures/Array/bench_sort.rb
@@ -0,0 +1,17 @@
+class Array
+  
+  def self.random(size)
+    Array.new(size){ Kernel.rand }
+  end
+  
+end
+
+require 'viiite'
+Viiite.bm do |b|
+  b.variation_point :ruby, Viiite.which_ruby
+  b.range_over([100, 1000], :size) do |size|
+    bench_case = Array.random(size)
+    b.report(:"Array#sort") { bench_case.sort }
+  end
+end
+
