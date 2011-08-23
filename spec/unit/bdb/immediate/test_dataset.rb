020033f4eb76787c9e17f93597647c6a7b7cbbbc
Bernard Lambeau <blambeau@gmail.com>
2011-08-23 13:18:48 +0200

    Removed the explicit rule

--- spec/unit/bdb/immediate/test_dataset.rb
+++ spec/unit/bdb/immediate/test_dataset.rb
@@ -6,11 +6,6 @@ module Viiite
       let(:bdb){ Immediate.new(fixtures_folder) }
       subject{ bdb.dataset(name) }
 
-      describe "when name denotes an existing file" do
-        let(:name){ File.expand_path('../existing.rash', __FILE__) }
-        specify{ subject.to_a.should eq([{:name => 'existing'}]) }
-      end
-
       describe "when the file exists in root folder (String)" do
         let(:name){ "bench_iteration" }
         it{ should be_a(Viiite::ViiiteFile) }
