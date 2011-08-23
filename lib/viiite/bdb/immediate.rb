a53304830878b7d0611334e4c63e87189e206c8d
Bernard Lambeau <blambeau@gmail.com>
2011-08-23 13:36:39 +0200

    Forgotten in previous commit

--- lib/viiite/bdb/immediate.rb
+++ lib/viiite/bdb/immediate.rb
@@ -8,9 +8,7 @@ module Viiite
       end
 
       def dataset(name)
-        if File.exists?(name.to_s)
-          return Alf::Reader.reader(name, self)
-        elsif File.exists?(file = bench_file(folder, name, ".rb"))
+        if File.exists?(file = bench_file(folder, name, ".rb"))
           return Alf::Reader.reader(file, self)
         else
           raise Alf::NoSuchDatasetError, "No such benchmark #{name}"
