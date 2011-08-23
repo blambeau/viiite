36d8bc15a5f31f95095af3bfe8e4b456c405add8
Bernard Lambeau <blambeau@gmail.com>
2011-08-23 15:53:04 +0200

    Split implementation of BDB::Cached#dataset

--- lib/viiite/bdb/cached.rb
+++ lib/viiite/bdb/cached.rb
@@ -11,19 +11,30 @@ module Viiite
         @cache_folder = cache_folder
       end
 
-      def dataset(name)
-        cached_file = bench_file(cache_folder, name, ".rash")
-        unless File.exists?(cached_file)
-          reader = delegate.dataset(name)
-          mkdir_p(File.dirname(cached_file))
-          File.open(cached_file, "w") do |io|
-            Alf::Renderer.rash(reader).execute(io)
-          end
-        end
-        Alf::Reader.reader(cached_file, self)
+      def benchmark(name)
+        delegate.benchmark(name)
       end
 
-      private 
+      def dataset(name)
+        cache_file = cache_file(name)
+        run_one!(name, "w") unless File.exists?(cache_file)
+        Alf::Reader.reader(cache_file, self)
+      end
+
+      private
+
+      def run_one!(name, mode)
+        cache_file = cache_file(name)
+        mkdir_p(File.dirname(cache_file))
+        reader = delegate.dataset(name)
+        File.open(cache_file(name), mode) do |io|
+          Alf::Renderer.rash(reader).execute(io)
+        end
+      end
+
+      def cache_file(name)
+        bench_file(cache_folder, name, ".rash")
+      end
       
       def mkdir_p(folder)
         require 'fileutils'
