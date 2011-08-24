6085cd9045cffc87e8802d26bad5ac5bc3581337
Bernard Lambeau <blambeau@gmail.com>
2011-08-24 13:45:41 +0200

    Added --runs and --run-key options to run

--- lib/viiite/command/run.rb
+++ lib/viiite/command/run.rb
@@ -12,14 +12,31 @@ module Viiite
     class Run < Quickl::Command(__FILE__, __LINE__)
       include Commons
 
+      options do |opt|
+        @runs = 1
+        opt.on("--runs=[NB]", 
+               "Run the benchmark NB times") do |val|
+          @runs = val.to_i
+          @run_key = :run unless @run_key
+        end
+        @run_key = nil
+        opt.on("--run-key=[KEY]", 
+               "Specify the run key (default to :run)") do |val|
+          @run_key = val
+        end
+      end
+
       def execute(argv)
         argv = requester.bdb.map{|t| t[:name]} if argv.empty?
         argv.each do |name|
-          $stderr << "Running #{name}" << "\n"
-          input = single_source([name]) do |bdb, arg|
+          benchmark = single_source([name]) do |bdb, arg|
             bdb.benchmark(arg)
           end
-          Alf::Renderer.rash(input).execute($stdout)
+          if @run_key
+            generator = Alf.lispy.generator(@runs, @run_key)
+            benchmark = Alf.lispy.join(benchmark, generator)
+          end
+          Alf::Renderer.rash(benchmark).execute($stdout)
         end
       end
 
--- spec/unit/command/test_run.rb
+++ spec/unit/command/test_run.rb
@@ -3,20 +3,37 @@ module Viiite
   class Command
     describe Run do
 
-      subject{ Run.run(argv) }
+      let(:bench_iteration){ File.expand_path('../bench_iteration.rb', __FILE__) }
+      subject{ 
+        Run.run(argv)
+        rel = Alf::Reader.reader(StringIO.new($stdout.string)).to_rel
+        rel = rel.project([:tms], {:allbut => true})
+      }
 
       before{ redirect_io }
       after { restore_io  }
 
       describe "when passed a benchmark file" do
-        let(:argv){ [ File.expand_path('../bench_iteration.rb', __FILE__) ] }
-        specify{
-          subject
-          $stdout.string.each_line do |line|
-            (h = eval(line)).should be_a(Hash)
-            h[:tms].should be_a(Viiite::Tms)
-          end
+        let(:argv){ [ bench_iteration ] }
+        it{ should eq(Alf::Relation[{:bench => :times}]) }
+      end
+
+      describe "with --runs" do
+        let(:argv){ [ bench_iteration, "--runs=2" ] }
+        let(:expected){ 
+          Alf::Relation[{:bench => :times, :run => 1}, 
+                        {:bench => :times, :run => 2}]
         }
+        it{ should eq(expected) }
+      end
+
+      describe "with --runs and --run-key" do
+        let(:argv){ [ bench_iteration, "--runs=2", "--run-key=hello" ] }
+        let(:expected){ 
+          Alf::Relation[{:bench => :times, :hello => 1}, 
+                        {:bench => :times, :hello => 2}]
+        }
+        it{ should eq(expected) }
       end
 
     end
