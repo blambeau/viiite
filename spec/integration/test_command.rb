4f77b5cf60c9fddc1a7e0b5fa64390eca208c034
eregon <eregontp@gmail.com>
2011-09-02 19:26:46 +0200

    simplify the viiite commands spec
    * RSpec output was kinda messy
    * fix the --suite parameter (it is actually unused, but might be useful to regenerate)

--- spec/integration/test_command.rb
+++ spec/integration/test_command.rb
@@ -1,30 +1,28 @@
 require 'spec_helper'
-describe "viiite command / " do
+describe "viiite commands" do
 
   Dir[File.expand_path('../**/*.cmd', __FILE__)].each do |input|
-    cmd = File.readlines(input).first
-    specify{ cmd.should =~ /^viiite / }
+    cmd = File.read(input).chomp
 
-    describe "#{File.basename(input)}: #{cmd}" do
-      let(:argv)     { Quickl.parse_commandline_args(cmd)[1..-1] }
-      let(:stdout)   { File.join(File.dirname(input), "#{File.basename(input, ".cmd")}.stdout") }
-      let(:stderr)   { File.join(File.dirname(input), "#{File.basename(input, ".cmd")}.stderr") }
-      let(:stdout_expected) { File.exists?(stdout) ? File.read(stdout) : "" }
-      let(:stderr_expected) { File.exists?(stderr) ? File.read(stderr) : "" }
+    specify "#{File.basename(input)}: #{cmd}" do
+      argv = Quickl.parse_commandline_args(cmd)[1..-1]
+      stdout = File.join(File.dirname(input), "#{File.basename(input, ".cmd")}.stdout")
+      stderr = File.join(File.dirname(input), "#{File.basename(input, ".cmd")}.stderr")
+      stdout_expected = File.exists?(stdout) ? File.read(stdout) : ""
+      stderr_expected = File.exists?(stderr) ? File.read(stderr) : ""
 
-      specify{
-        cache = File.join(fixtures_folder, "saved")
-        out, err = capture_io do
-          begin
-            Viiite::Command.run(["--suite=#{cache}", "--cache=#{cache}"] + argv)
-          rescue SystemExit
-            $stdout << "SystemExit" << "\n"
-          end
+      cmd.should match /^viiite /
+      cache = File.join(fixtures_folder, "saved")
+      out, err = capture_io do
+        begin
+          Viiite::Command.run(["--suite=#{fixtures_folder}/bdb", "--cache=#{cache}"] + argv)
+        rescue SystemExit
+          $stdout << "SystemExit" << "\n"
         end
+      end
 
-        out.should eq stdout_expected unless RUBY_VERSION < "1.9"
-        err.should eq stderr_expected unless RUBY_VERSION < "1.9"
-      }
+      out.should eq stdout_expected unless RUBY_VERSION < "1.9"
+      err.should eq stderr_expected unless RUBY_VERSION < "1.9"
     end
   end
 
