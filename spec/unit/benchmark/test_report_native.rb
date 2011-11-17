101bba4ce7c90b6a448d11595d4f78690c1ea193
Bernard Lambeau <blambeau@gmail.com>
2011-11-17 12:18:57 +0100

    Test environment vars are correctly passed

--- spec/unit/benchmark/test_report_native.rb
+++ spec/unit/benchmark/test_report_native.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 module Viiite
   describe Benchmark, "report_native" do
 
-    let(:envi){ Hash.new }
+    let(:env){ Hash.new }
     let(:options){ Hash.new }
     let(:bench) {
       Viiite.bench do |b|
-        b.report_native(envi, command, options)
+        b.report_native(env, command, options)
       end
     }
     subject{ bench.to_a }
@@ -30,6 +30,12 @@ module Viiite
     context "when called without a parsing block" do
       let(:command){ "echo '1.10'" }
       it{ should eq([{:tms => Tms.coerce(1.10)}]) }
+    end
+
+    context "when setting an environment var" do
+      let(:env){ {'VIIITENV' => '2.0'} }
+      let(:command){ %q{ruby -e "puts ENV['VIIITENV']"} }
+      it{ should eq([{:tms => Tms.coerce(2.0)}]) }
     end
 
   end
