6bf7314115724b2e61c6d55bd89c39aeac40cdeb
Bernard Lambeau <blambeau@gmail.com>
2012-01-27 15:11:10 +0100

    Reporter handles the cache as well

--- lib/viiite/reporter.rb
+++ lib/viiite/reporter.rb
@@ -5,25 +5,40 @@ module Viiite
 
     def initialize(config)
       @config = config
+      @ios    = [config.stdout].compact
     end
 
     def report(unit)
       case unit
+      when Benchmark
+        with_ios(unit) do 
+          unit.run.each &method(:call)
+        end
       when Suite
         unit.each.run self
       else
-        unit.each &method(:call)
+        raise ArgumentError, "Unable to report on #{unit}"
       end
-    end
-
-    def ios
-      [config.stdout]
     end
 
     def call(tuple)
       literal = Alf::Tools.to_ruby_literal(tuple)
-      ios.each do |io|
+      @ios.each do |io|
         io << literal << "\n"  
+      end
+    end
+
+    private
+
+    def with_ios(bench)
+      if cache = config.cache_file_for(bench)
+        cache.open('a') do |io|
+          @ios.push(io)
+          yield
+          @ios.pop
+        end
+      else
+        yield
       end
     end
 
