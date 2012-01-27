8fd40bed619a32246651ebefd49e0c6b767cd0bf
Bernard Lambeau <blambeau@gmail.com>
2012-01-27 17:58:38 +0100

    Suite should not know about files

--- lib/viiite/suite.rb
+++ lib/viiite/suite.rb
@@ -3,35 +3,14 @@ module Viiite
     include Unit
 
     attr_reader :config
+    attr_reader :benchmarks
 
-    def initialize(config, files = config.benchmark_folder)
-      @config = config
-      @files  = Array(files).map{|f| Path(f).expand}
-    end
-
-    def benchmark_files
-      benchmarks.map(&:path)
+    def initialize(config, benchmarks)
+      @config     = config
+      @benchmarks = benchmarks
     end
 
     protected
-    attr_reader :files
-
-    def benchmarks
-      files.map(&method(:load_one)).flatten.compact
-    end
-
-    def load_one(file)
-      if file.file?
-        unless bench = Viiite.bench(config, file)
-          warn "No benchmark found in #{file}"
-        end
-        [ bench ]
-      else
-        file.glob(config.benchmark_pattern).
-             sort.
-             map{|file| load_one(file)}
-      end
-    end
 
     def _run(extra, reporter)
       benchmarks.each do |unit|
--- spec/unit/test_suite.rb
+++ spec/unit/test_suite.rb
@@ -3,35 +3,12 @@ module Viiite
   describe Suite do
 
     let(:config){ fixtures_config }
+    let(:bench){ Viiite.bench{|b| b.report{}} }
+    let(:suite){ Suite.new(config, [bench])   }
+    
+    subject{ suite }
 
-    context "on a single file" do
-      let(:subject){ Suite.new(config, config.benchmark_folder/"bench_iteration.rb") }
-      it_should_behave_like "A Unit"
-      it 'contains the expected benchmark' do
-        subject.benchmark_files.should eq([config.benchmark_folder/"bench_iteration.rb"])
-      end
-    end
-
-    context "on a folder" do
-      let(:subject){ Suite.new(config, config.benchmark_folder) }
-      it_should_behave_like "A Unit"
-      it 'contains the expected benchmarks' do
-        subject.benchmark_files.should eq([
-          config.benchmark_folder/"Array/bench_sort.rb",
-          config.benchmark_folder/"bench_iteration.rb"
-        ])
-      end
-    end
-
-    context "on a wrong file" do
-      let(:subject){ Suite.new(config, fixtures_folder/"fake_bench.rb") }
-      it 'contains the expected benchmarks' do
-        subject.benchmark_files.should eq([])
-      end
-      it 'should not raise errors' do
-        subject.run{|t| }
-      end
-    end
+    it_should_behave_like "A Unit"
 
   end # describe Suite
 end # module Viiite
