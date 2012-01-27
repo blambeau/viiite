0b5398a83b71001c46edbdab371a1e1765179d30
Bernard Lambeau <blambeau@gmail.com>
2012-01-27 11:43:50 +0100

    Add a Suite helper

---
+++ lib/viiite/suite.rb
@@ -0,0 +1,42 @@
+module Viiite
+  class Suite
+    include Enumerable
+    
+    attr_reader :config
+    attr_reader :path
+    
+    def initialize(config, path = config.benchmark_folder)
+      @config = config
+      @path   = path
+    end
+    
+    def run(&reporter)
+      each do |bench|
+        bench.run(&reporter)
+      end
+    end
+    
+    def each(&proc)
+      benchmarks = if path.file?
+        [ load_one(path) ]
+      else
+        path.glob(config.benchmark_pattern).
+             map{|file| load_one(file)}
+      end
+      benchmarks.compact.each(&proc)
+    end
+    
+    def empty?
+      to_a.empty?
+    end
+    
+    private
+    
+    def load_one(file)
+      bench = Benchmark.new(file)
+      warn "No benchmark found in #{file}" unless bench
+      bench
+    end
+    
+  end # class Suite
+end # module Viiite
---
+++ spec/unit/test_suite.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+module Viiite
+  describe Suite do
+    
+    let(:config){ fixtures_config }
+    
+    shared_examples_for("A benchmark suite") do
+
+      it{ should be_a(Enumerable) }
+
+      it{ should_not be_empty     }
+
+      it 'should contain Benchmark instances only' do
+        subject.all?{|b| b.is_a?(Benchmark)}.should be_true
+      end
+
+      it 'should run without failure' do
+        seen = []
+        subject.run do |t| seen << t; end
+        seen.should_not be_empty
+      end
+    end
+
+    context "on a single file" do
+      let(:subject){ Suite.new(config, config.benchmark_folder/"bench_iteration.rb") }
+
+      it_should_behave_like("A benchmark suite")
+
+      it 'should contain one Benchmark only' do
+        subject.to_a.size.should eq(1)
+      end
+
+      it 'should contain the expected benchmark' do
+        subject.to_a.first.path.should eq(config.benchmark_folder/"bench_iteration.rb")
+      end
+    end
+    
+    context "on a folder" do
+      let(:subject){ Suite.new(config, config.benchmark_folder) }
+
+      it_should_behave_like("A benchmark suite")
+    end
+
+    context "on a wrong file" do
+      let(:subject){ Suite.new(config, fixtures_folder/"fake_bench.rb") }
+      it{ should be_empty }
+    end
+    
+  end # describe Suite
+end # module Viiite
